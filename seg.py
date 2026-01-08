620f06b60f8c52be8a1ba424f9d0ec4fcc9bebb0
Derek Enlow <derekenlow@gmail.com>
2026-01-08 03:55:58 -0700

    Segmentation output retains first-insertion order.

--- seg.py
+++ seg.py
@@ -3,7 +3,7 @@ import pyhanlp
 
 
 def segment(path: pathlib.Path) -> str:
-    word_set = set()
+    word_set = dict()
     with open(path, encoding="utf-8") as subtitle_text:
         for line in subtitle_text:
             line = line.strip("\n")
@@ -13,9 +13,9 @@ def segment(path: pathlib.Path) -> str:
                         str(term.nature) not in set(["w", "nx"])
                         and not str(term.word).isdigit()
                     ):
-                        word_set.add((str(term.word), str(term.nature)))
-    print(len(word_set))
+                        if str(term.word) not in word_set:
+                            word_set[str(term.word)] = True
     # for word in word_set:
     #     if word[1] == "e":
     #         print(word)
-    return "\n".join([word[0] for word in word_set])
+    return "\n".join(word_set)
